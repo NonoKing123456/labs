3011ac5e757b8cb9d47d135c7df05c0d61b26430
nono <fangyiheng38@gmail.com>
2025-09-15 22:01:12 -0400

    part2 finished

--- lab1/mini/part2.c
+++ lab1/mini/part2.c
@@ -11,8 +11,8 @@
 void
 set_point(struct point *pt, double x, double y)
 {
-	// TODO: Your code here.
-	assert(0);
+	pt->x = x;   // ✅ assign to struct field
+    pt->y = y;
 }
 
 // Compute the planar distance between two points. Recall that the
@@ -27,7 +27,7 @@ set_point(struct point *pt, double x, double y)
 double
 point_dist(struct point *pt1, struct point *pt2)
 {
-	// TODO: Your code here.
-	assert(0);
-	return 0.0;
+	double dx = pt1->x - pt2->x;
+    double dy = pt1->y - pt2->y;
+    return sqrt(dx * dx + dy * dy);   // ✅ distance formula
 }
